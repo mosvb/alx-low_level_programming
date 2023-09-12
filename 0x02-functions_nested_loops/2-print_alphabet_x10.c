d9e76735f82646d576ef1657eaff435306dc67fb
mosvb <mosab.x.khaled66@gmail.com>
2023-09-12 14:48:40 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,28 @@
 
 void print_alphabet_x10(void)
 {
-char ch;
-int i;
-i = 0;
-while (i < 10)
-{
-ch = 'a';
-while (ch <= 'z')
+
+	char ch;
+
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+
+		ch = 'a';
+
+		while (ch <= 'z')
 		{
-_putchar(ch);
-ch++;
+
+			_putchar(ch);
+
+			ch++;
 		}
-_putchar('\n');
-i++;
+
+		_putchar('\n');
+
+		i++;
        	}
 }
