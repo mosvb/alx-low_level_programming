2ca1952850093885a211d9bf689cebd3a5b9b709
mosvb <mosab.x.khaled66@gmail.com>
2023-09-12 14:00:58 -0700

    update2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,15 +10,13 @@
 void print_alphabet(void)
 
 {
+	char letter;
 
-        char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
 
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-
-                _putchar(letter);
-
-
-        _putchar('\n');
+		_putchar(letter);
+	
+	_putchar('\n');
 
 }
